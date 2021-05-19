37fd9f3bc2eced119cf84a859db475e3f6a9bf6e
Ferran Iglesias Barenys <74904071+Ferran00@users.noreply.github.com>
2021-05-19 17:43:10 +0200

    fent arreglaciones

--- Event.py
+++ Event.py
@@ -1,16 +1,14 @@
 
-
-
 class Event:
     # creació d'un nou esdeveniment
-    def __init__(self,object,type,time,entity):
-        #objecte que processarà l'esdeveniment
-        self.object = object;
-        #tipus event
-        self.type = type;
-        #instant en que succeirà l'esdeveniment
-        self.time = time;
-        
-    #Podríem delegar l'esdeveniment a l'objecte des de l'event o des del scheduler
+    def __init__(self, objecte, tipus, time, entity):
+        # objecte que processarà l'esdeveniment
+        self.object = objecte
+        # tipus event
+        self.type = tipus
+        # instant en que succeirà l'esdeveniment
+        self.time = time
+
+    # Podríem delegar l'esdeveniment a l'objecte des de l'event o des del scheduler
     def tractaresdeveniment(self):
-        self.object.tractaresdeveniment(self);
+        self.object.tractaresdeveniment(self)
--- Source.py
+++ Source.py
@@ -1,50 +1,49 @@
-#millor treballar amb define o algun sistema simular a l'enum de C++
-from enumeracions import *
+# millor treballar amb define o algun sistema simular a l'enum de C++
+# from enumeracions import *
 from Server import *
-from scipy.stats import triang 
+from scipy.stats import triang
+
 
 class Source:
 
-    def __init__(self,scheduler):
+    def __init__(self, scheduler):
         # inicialitzar element de simulació
-        entitatsCreades=0
-        self.state=idle
-        self.scheduler=scheduler
-    
-    def crearConnexio(self,server):
-        self.server=server     
+        entitatsCreades = 0
+        self.state = idle
+        self.scheduler = scheduler
+
+    def crearConnexio(self, server):
+        self.server = server
 
     def tractarEsdeveniment(self, event):
-        if (event.tipus=='SIMULATION START'):
+        if (event.tipus == 'SIMULATION START'):
             self.simulationStart(event)
 
-        if (event.tipus=='NEXT ARRIVAL'):
+        if (event.tipus == 'NEXT ARRIVAL'):
             self.processNextArrival()
-  
 
-    def simulationStart(self,event):
-        nouEvent=self.properaArribada(0)
+    def simulationStart(self, event):
+        nouEvent = self.properaArribada(0)
         self.scheduler.afegirEsdeveniment(nouEvent)
 
-    def processNextArrival(self,event):
+    def processNextArrival(self, event):
         # Cal crear l'entitat 
-        entitat=self.crearEntitat(self)
+        entitat = self.crearEntitat(self)
         # Mirar si es pot transferir a on pertoqui
-        #transferir entitat (es pot fer amb un esdeveniment immediat o invocant a un métode de l'element)
-        server.recullEntitat(event.time,entitat)
+        # transferir entitat (es pot fer amb un esdeveniment immediat o invocant a un métode de l'element)
+        server.recullEntitat(event.time, entitat)
         # Cal programar la següent arribada
-        nouEvent=self.properaArribada(event.temps)
+        nouEvent = self.properaArribada(event.temps)
         self.scheduler.afegirEsdeveniment(nouEvent)
 
     def properaArribada(self, time):
         # cada quan generem una arribada (aleatorietat)
         tempsEntreArribades = self.calcularTempsEntreArribades()
         # incrementem estadistics si s'escau
-        self.entitatsCreades=self.entitatsCreades+1
+        self.entitatsCreades = self.entitatsCreades + 1
         self.state = busy
         # programació primera arribada
-        return Event(self,'NEXT ARRIVAL', time+ tempsEntreArribades,null)   
+        return Event(self, 'NEXT ARRIVAL', time + tempsEntreArribades, null)
 
-    def calcularTempsEntreArribades:
-        return tempsEntreArribades() #implementar distribució del inter-arrival time dels sources triangular(129.0, 167.0, 148.0, getstream(current))
-         
+    def calcularTempsEntreArribades(self):
+        return 3  # tempsEntreArribades() #implementar distribució del inter-arrival time dels sources triangular(129.0, 167.0, 148.0, getstream(current))
