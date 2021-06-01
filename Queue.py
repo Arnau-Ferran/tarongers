296187fea95fea404f3f20cb3a1b27046eb2489b
Ferran Iglesias Barenys <74904071+Ferran00@users.noreply.github.com>
2021-06-01 18:20:23 +0200

    queue podriem dir que done excepte pq no he testejat res

--- Queue.py
+++ Queue.py
@@ -2,7 +2,6 @@
 class Queue:
     empaquetador = None
     size = None
-    # TODO estadistics
 
     def __init__(self, scheduler):
         self.scheduler = scheduler
@@ -15,21 +14,29 @@ class Queue:
         self.empaquetador = nou_empaquetador
 
     def tractar_esdeveniment(self, event):
+        #when arriba una nova taronja
         if event.type == 'END_TRANSPORT':
             #if self.state != "empty":
             print("Queue got END_TRANSPORT and it is on "+self.state)
             #else:
             self.processarEndTransport(event)
 
+        #when l'empaquetador acaba
         elif event.type == 'END_EMPAQUETAMENT':
             print("Queue got END_EMPAQUETAMENT and it is on "+self.state)
             self.processarEndEmpaquetament(event)
 
     def processarEndTransport(self, event):
+        #si li puc passar directament a l'empaquetador
         if self.empaquetador.getState == "idle":
             #li passo al empaquetador el min entre les q li falten i les q li puc passar.
-            self.empaquetador.arribenTaronges(min(50 - self.empaquetador.getSize), event.numTaronges)       # nose si el max i min funcionen. i en general aixo es pot fer mes bonic
-            #després de passar endevant totes les q puc, les q queden se'm sumen. son o 0 o les q m'ha arribat - les q he passat endavant.
+            n_taronges_passo = min((50 - self.empaquetador.getSize), event.numTaronges)       # nose si el max i min funcionen. i en general aixo es pot fer mes bonic
+            self.empaquetador.arribenTaronges(n_taronges_passo)
+
+            #estadístics
+            self.sumaTarongesOutput = self.sumaTarongesOutput + n_taronges_passo
+
+            #després de passar endevant totes les q puc, les que queden se'm sumen. son o 0 o les q m'ha arribat - les q he passat endavant.
             self.size = self.size + max(0, event.numTaronges-(50 - self.empaquetador.getSize))
             if self.size == 0:
                 self.state="empty"
@@ -40,7 +47,13 @@ class Queue:
             self.state = "notempty"
 
     def processarEndEmpaquetament(self, event):
-        self.empaquetador.arribenTaronges(self.size)    #ojo!!
+        n_taronges_passo = min(50, self.size)
+
+        self.empaquetador.arribenTaronges(n_taronges_passo)    #ojo!! he fet aixo aqui.
+
+        #estadístics
+        self.sumaTarongesOutput = self.sumaTarongesOutput + n_taronges_passo
+
         self.size = max(0, self.size-50)
 
         if self.size == 0:
